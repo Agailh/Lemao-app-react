b7c886591d89bc754caaea1089d2b88fbe3467b6
Agailh <agailham86@gmail.com>
2023-01-16 16:00:27 +0700

    about

---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import BGh from "../assets/Kaela_HolidayGift.jpg";
+
+const About = () => {
+  return (
+    <div Name="About">
+      <div className="w-full my-40">
+        <div className="bg-cover bg-center">
+          <img src={BGh} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
